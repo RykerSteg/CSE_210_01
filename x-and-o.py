96bbdf9bcef271de03af09e915a66936501633df
RykerSteg <Rykersteglich@gmail.com>
2022-01-11 08:21:23 -0700

    update to winner function

--- x-and-o.py
+++ x-and-o.py
@@ -4,17 +4,21 @@
 def main():
   board = newBoard()
   displayBoard(board)
-  game = "play"
+  game = 0
   
-  while game != "winner" or game != "tie":
+  while game != 1 or game != 2:
     xturn = input("x's turn to choose a square (1-9):")
     playerOne(xturn, board)
     displayBoard(board)
-    game = winner(board, game)
+    game = winner(board)
+    if game == 1 or game == 2:
+      break
     oturn = input("o's turnt to choose a sqaure (1-9):")
     playerTwo(oturn, board)
     displayBoard(board)
-    game = winner(board, game)
+    game = winner(board)
+    if game == 1 or game == 2:
+      break
   print("Good game. Thanks for playing!")
 
 
@@ -43,8 +47,9 @@ def playerTwo(turn, board):
   board[spot] = "o"
   return board
 
-def winner(board, game):
+def winner(board):
   if board[0] == board[1] == board[2] or board[0] == board[3] == board[6] or board[0] == board[4] == board[8] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[6] == board[4] == board[2]:
-      game = "winner"
-      return game
+      winner = 1
+      return winner
+  
 main()
