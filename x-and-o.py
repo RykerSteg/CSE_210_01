c85b4757dd845446ed8c9367e79560d12bf5a6ad
RykerSteg <Rykersteglich@gmail.com>
2022-01-11 08:03:28 -0700

    Initialize Repo

---
+++ x-and-o.py
@@ -0,0 +1,50 @@
+"Tic-Tac-Toe"
+"By Ryker Steglich"
+
+def main():
+  board = newBoard()
+  displayBoard(board)
+  game = "play"
+  
+  while game != "winner" or game != "tie":
+    xturn = input("x's turn to choose a square (1-9):")
+    playerOne(xturn, board)
+    displayBoard(board)
+    game = winner(board, game)
+    oturn = input("o's turnt to choose a sqaure (1-9):")
+    playerTwo(oturn, board)
+    displayBoard(board)
+    game = winner(board, game)
+  print("Good game. Thanks for playing!")
+
+
+
+  
+def newBoard():
+  board = [1,2,3,4,5,6,7,8,9]
+  return board
+
+def displayBoard(board):
+  row = "--+-+--"
+  print()
+  print(f"|{board[0]}|{board[1]}|{board[2]}")
+  print(f"{row}")
+  print(f"|{board[3]}|{board[4]}|{board[5]}")
+  print(f"{row}")
+  print(f"|{board[6]}|{board[7]}|{board[8]}")
+
+def playerOne(turn, board):
+  spot = int(turn) - 1
+  board[spot] = "x"
+  return board
+  
+def playerTwo(turn, board):
+  spot = int(turn) - 1
+  board[spot] = "o"
+  return board
+
+def winner(board, game):
+  if board[0] == board[1] == board[2] or board[0] == board[3] == board[6] or board[0] == board[4] == board[8] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[6] == board[4] == board[2]:
+      game = "winner"
+      return game
+main()
