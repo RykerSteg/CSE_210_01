0c63e4eebeba492a09432d184250c1cb8e769a8e
RykerSteg <Rykersteglich@gmail.com>
2022-01-13 22:43:05 -0700

    added functions to verify wins and ties

--- x-and-o.py
+++ x-and-o.py
@@ -4,20 +4,20 @@
 def main():
   board = newBoard()
   displayBoard(board)
-  game = 0
+  game = "play"
   
-  while game != 1 or game != 2:
+  while game != "win" or game != "tie":
     xturn = input("x's turn to choose a square (1-9):")
     playerOne(xturn, board)
     displayBoard(board)
     game = winner(board)
-    if game == 1 or game == 2:
+    if game == "win" or game == "tie":
       break
     oturn = input("o's turnt to choose a sqaure (1-9):")
     playerTwo(oturn, board)
     displayBoard(board)
     game = winner(board)
-    if game == 1 or game == 2:
+    if game == "win" or game == "tie":
       break
   print("Good game. Thanks for playing!")
 
@@ -29,27 +29,30 @@ def newBoard():
   return board
 
 def displayBoard(board):
-  row = "--+-+--"
+  row = "-+-+-"
   print()
-  print(f"|{board[0]}|{board[1]}|{board[2]}")
+  print(f"{board[0]}|{board[1]}|{board[2]}")
   print(f"{row}")
-  print(f"|{board[3]}|{board[4]}|{board[5]}")
+  print(f"{board[3]}|{board[4]}|{board[5]}")
   print(f"{row}")
-  print(f"|{board[6]}|{board[7]}|{board[8]}")
+  print(f"{board[6]}|{board[7]}|{board[8]}")
 
 def playerOne(turn, board):
   spot = int(turn) - 1
-  board[spot] = "x"
+  board[spot] = "X"
   return board
   
 def playerTwo(turn, board):
   spot = int(turn) - 1
-  board[spot] = "o"
+  board[spot] = "O"
   return board
 
 def winner(board):
+  tie = [1,2,3,4,5,6,7,8,9]
   if board[0] == board[1] == board[2] or board[0] == board[3] == board[6] or board[0] == board[4] == board[8] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[6] == board[4] == board[2]:
-      winner = 1
+      winner = "win"
       return winner
-  
+  elif board[0] != tie[0] and board[1] != tie[1] and board[2] != tie[2] and board[3] != tie[3] and board[4] != tie[4] and board[5] != tie[5] and board[6] != tie[6] and board[7] != tie[7] and board[8] != tie[8]:
+    stalemate = "tie"
+    return stalemate
 main()
